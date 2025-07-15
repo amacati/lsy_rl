Fix scaling of logstds for sac and ppo. Update initial value for fixed logstds in ppo

lsy_rl/ppo/policy.py
@@ -44,7 +44,7 @@ class PPOActorNet(nn.Module):
                 "f_out": nn.Identity(),
             }
         )
-        self.logstd = nn.Parameter(torch.zeros(1, torch.tensor(action_shape).prod()))
+        self.logstd = nn.Parameter(-2*torch.ones(1, torch.tensor(action_shape).prod()))
 
     def forward(self, obs: Tensor) -> tuple[Tensor, Tensor]:
         x = obs.float()
@@ -96,7 +96,7 @@ class PPOActorNetWithStd(nn.Module):
         for layer in self.logstd_head.values():
             logstd = layer(logstd)
         # Same method used in SAC's implementation for more stable training
-        logstd = self.LOG_STD_MIN + 0.5 * (self.LOG_STD_MAX - self.LOG_STD_MIN) * logstd + 1
+        logstd = self.LOG_STD_MIN + 0.5 * (self.LOG_STD_MAX - self.LOG_STD_MIN) * (logstd + 1)
         return mean, logstd
     
 

lsy_rl/sac/policy.py
@@ -68,7 +68,7 @@ class SACActorNet(nn.Module):
         for layer in self.logstd_layer.values():
             logstd = layer(logstd)
         # From SpinUp / Denis Yarats
-        logstd = self.LOG_STD_MIN + 0.5 * (self.LOG_STD_MAX - self.LOG_STD_MIN) * logstd + 1
+        logstd = self.LOG_STD_MIN + 0.5 * (self.LOG_STD_MAX - self.LOG_STD_MIN) * (logstd + 1)
         return mean, logstd
 
 