00f9cdbdd3a0fe1ffcd81d2e1b8439c98ba0c0ae
Sherif-Sameh <sherif252525@gmail.com>
2025-08-06 12:19:08 +0200

    Update replay buffer load method. Fix asserts when loading replay buffer

--- lsy_rl/core/replay_buffer.py
+++ lsy_rl/core/replay_buffer.py
@@ -147,7 +147,8 @@ class SimpleReplayBuffer(ReplayBuffer):
         Args:
             path: The path to the file.
         """
-        save_dict = torch.load(path, map_location=self.device)
+        # Note: weights_only=False is required to recover the buffer when loading
+        save_dict = torch.load(path, map_location=self.device, weights_only=False)
         self._idx, self._maxidx = save_dict["idx"], save_dict["maxidx"]
         self.buffer = save_dict["buffer"]
         assert self.buffer.batch_size == self.max_size, "Loaded buffer has wrong size"
@@ -217,9 +218,10 @@ class TrajectoryBuffer(ReplayBuffer):
         Args:
             path: The path to the file.
         """
-        save_dict = torch.load(path, map_location=self.device)
+        # Note: weights_only=False is required to recover the buffer when loading
+        save_dict = torch.load(path, map_location=self.device, weights_only=False)
         self._idx, self.buffer = save_dict["idx"], save_dict["buffer"]
-        assert self.buffer.batch_size == self.bufflen, "Loaded buffer has wrong size"
+        assert self.buffer.batch_size == (self.trajectory_len, self.num_envs), "Loaded buffer has wrong size"
 
     def full(self) -> bool:
         """Check if the buffer is full."""
@@ -345,10 +347,11 @@ class VectorReplayBuffer(ReplayBuffer):
         Args:
             path: The path to the file.
         """
-        save_dict = torch.load(path, map_location=self.device)
+        # Note: weights_only=False is required to recover the buffer when loading
+        save_dict = torch.load(path, map_location=self.device, weights_only=False)
         self._idx, self._maxidx = save_dict["idx"], save_dict["maxidx"]
         self.buffer = save_dict["buffer"]
-        assert self.buffer.batch_size == self.bufflen, "Loaded buffer has wrong size"
+        assert self.buffer.batch_size == (self.num_envs, self.bufflen), "Loaded buffer has wrong size"
 
     def __len__(self) -> int:
         """Return the number of valid samples in the buffer."""
